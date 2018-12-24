6cfbf5492270d5eecb2592f4ee2c7910760197f5
S0019881971 <chaitanya_anisetti@toyota.ca>
2018-12-24 20:10:15 +0000

    XSJS Changes

--- vehicleLocator_XSJS/server.js
+++ vehicleLocator_XSJS/server.js
@@ -1,30 +1,40 @@
 /*eslint no-console: 0, no-unused-vars: 0*/
 "use strict";
 
-var xsjs  = require("@sap/xsjs");
+var xsjs = require("@sap/xsjs");
 var xsenv = require("@sap/xsenv");
-var port  = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var options = {
-	anonymous : true, // remove to authenticate calls
-	redirectUrl : "/index.xsjs"
+	anonymous: true, // remove to authenticate calls
+	redirectUrl: "/index.xsjs"
 };
 
 // configure HANA
 try {
-	options = Object.assign(options, xsenv.getServices({ hana: {tag: "hana"} }));
+	options = Object.assign(options, xsenv.getServices({
+		hana: {
+			tag: "hana"
+		}
+	}));
 } catch (err) {
 	console.log("[WARN]", err.message);
 }
 
 // configure UAA
-try {
-	options = Object.assign(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
+/*try {
+	options = Object.assign(options, xsenv.getServices({
+		uaa: {
+			tag: "xsuaa"
+		}
+	}));
 } catch (err) {
 	console.log("[WARN]", err.message);
 }
-
+*/
 // start server
 xsjs(options).listen(port);
 
 console.log("Server listening on port %d", port);
+
+//  all my xjss odata will be part of this.
