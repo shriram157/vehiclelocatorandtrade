8b598ded6b2f8164d61fc7f567d5c2d1a731f6e5
Anthony Wat <anthony_wat@toyota.ca>
2019-04-16 10:35:55 -0400

    Added more logging + adjusted logic for trade req cleanup task

--- vehicleLocator_Node/core/trade-req-cleanup-task.js
+++ vehicleLocator_Node/core/trade-req-cleanup-task.js
@@ -8,8 +8,8 @@ function run(client, appContext) {
 	var tracer = appContext.createLogContext().getTracer(__filename);
 	return new Promise((resolve, reject) => {
 		var today = moment(new Date()).format("YYYY-MM-DD");
-		var selectSql = "SELECT \"Trade_Id\", \"Requested_Dealer\", \"Requested_Vtn\" FROM " +
-			"\"vehicleTrade.Trade_Request\" WHERE \"Trade_Status\" = 'A' AND \"Changed_on\" = ?";
+		var selectSql = "SELECT \"Trade_Id\", \"Requested_Dealer\", \"Requested_Vtn\", \"Requesting_Dealer\", " +
+			"\"Offered_Vtn\" FROM \"vehicleTrade.Trade_Request\" WHERE \"Trade_Status\" = 'A' AND \"Changed_on\" = ?";
 		client.prepare(selectSql, function (err, selectStmt) {
 			if (err) {
 				tracer.error("ERROR preparing SQL statement [ %s ] with error [ %s ].", selectSql, err.toString());
@@ -17,6 +17,7 @@ function run(client, appContext) {
 				return;
 			}
 			var selectStmtParams = [today];
+			tracer.debug("Executing SQL statement [ %s ] with parameters [ %s ].", selectSql, JSON.stringify(selectStmtParams));
 			selectStmt.exec(selectStmtParams, function (err, selectResults) {
 				if (err) {
 					tracer.error("ERROR executing SQL statement [ %s ] with parameters [ %s ] and error [ %s ].", selectSql, JSON.stringify(
@@ -24,13 +25,16 @@ function run(client, appContext) {
 					reject(new Error(err.toString()));
 					return;
 				}
-				if (selectResults.length == 0) {
+				if (selectResults.length === 0) {
 					logger.info("[TRADE_REQ_CLEANUP_TASK] No trade requests accepted today found.");
 					resolve();
 					return;
+				} else {
+					logger.info("[TRADE_REQ_CLEANUP_TASK] Found %s trade requests accepted today to be cleaned up for.", selectResults.length);
 				}
 				var updateSql = "UPDATE \"vehicleTrade.Trade_Request\" SET \"Trade_Status\" = 'R', \"Changed_on\" = ?" +
-					" WHERE \"Requested_Dealer\" = ? AND \"Requested_Vtn\" = ? AND \"Trade_Status\" NOT IN ('A', 'R')";
+					" WHERE ((\"Requested_Dealer\" = ? AND \"Requested_Vtn\" = ?) OR (\"Requesting_Dealer\" = ? AND " +
+					"\"Offered_Vtn\" = ?)) AND \"Trade_Status\" IN ('C', 'S')";
 				client.prepare(updateSql, function (err, updateStmt) {
 					if (err) {
 						tracer.error("ERROR preparing SQL statement [ %s ] with error [ %s ].", updateSql, err.toString());
@@ -42,13 +46,22 @@ function run(client, appContext) {
 					for (let i = 0; i < selectResults.length; i++) {
 						updatePromises.push(new Promise((resolve, reject) => {
 							var selectResult = selectResults[i];
-							var updateStmtParams = [today, selectResult.Requested_Dealer, selectResult.Requested_Vtn];
+							var updateStmtParams = [
+								today,
+								selectResult.Requested_Dealer,
+								selectResult.Requested_Vtn,
+								selectResult.Requesting_Dealer,
+								selectResult.Offered_Vtn
+							];
+							tracer.debug("Executing SQL statement [ %s ] with parameters [ %s ].", updateSql, JSON.stringify(updateStmtParams));
 							updateStmt.exec(updateStmtParams, function (err, updateResults) {
 								if (err) {
 									tracer.error("ERROR executing SQL statement [ %s ] with parameters [ %s ] and error [ %s ].", updateSql, JSON.stringify(
 										updateStmtParams), err.toString());
 									reject(new Error(err.toString()));
 								}
+								logger.info("[TRADE_REQ_CLEANUP_TASK] Cleaned up %s stale trade requests associated with trade request %s.",
+									updateResults, selectResult.Trade_Id);
 								resolve();
 								return;
 							});
