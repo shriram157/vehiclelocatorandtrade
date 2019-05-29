c6114ef6f11ce6942b2bf98d98ef906ae18b99a6
Anthony Wat <anthony_wat@toyota.ca>
2019-05-29 19:13:45 -0400

    Revert "Fixed i18n loading for session t/o dialog based on Language URL param"
    This reverts commit 257d2804b046b315caa9d1fcea800a7393d41646.

--- vehicleLocator/webapp/Component.js
+++ vehicleLocator/webapp/Component.js
@@ -1,11 +1,10 @@
 sap.ui.define([
-	"sap/base/i18n/ResourceBundle",
 	"sap/m/Dialog",
 	"sap/m/Text",
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
 	"vehicleLocator/model/models"
-], function (ResourceBundle, Dialog, Text, UIComponent, Device, models) {
+], function (Dialog, Text, UIComponent, Device, models) {
 	"use strict";
 
 	return UIComponent.extend("vehicleLocator.Component", {
@@ -30,13 +29,7 @@ sap.ui.define([
 			this.setModel(models.createDeviceModel(), "device");
 
 			// Get resource bundle
-			var locale = jQuery.sap.getUriParameters().get('Language');
-			var bundle = !locale ? ResourceBundle.create({
-				url: './i18n/i18n.properties'
-			}): ResourceBundle.create({
-				url: './i18n/i18n.properties',
-				locale: locale
-			});
+			var bundle = this.getModel('i18n').getResourceBundle();
 
 			// Attach XHR event handler to detect 401 error responses for handling as timeout
 			var sessionExpDialog = new Dialog({
