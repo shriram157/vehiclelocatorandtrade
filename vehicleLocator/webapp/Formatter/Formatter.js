23b6be5b02590046db3b79a9bcad88815c2c3193
Nalla Govindareddy Gar Gunasekhar Reddy <gunasekhar_reddy2@toyota.ca>
2019-06-18 01:43:31 +0000

    Fixing the Syntax for Model Block Date

--- vehicleLocator/webapp/Formatter/Formatter.js
+++ vehicleLocator/webapp/Formatter/Formatter.js
@@ -317,17 +317,23 @@ sap.ui.define(function () {
 		},
 		
 			TradeSummaryoDateModelBlock: function (Created_On) {
-							var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({
-					pattern: "yyyy-MM-dd"
-				});
-			    var dateTo = oDateFormat.format(Created_On);
+				// 			var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({
+				// 	pattern: "yyyy-MM-dd"
+				// });
+				
+				var ValidTo = new Date(Created_On)
+				var returnThisDate = moment(ValidTo).format("YYYY-MM-DD");
+				
+				
+				
+			  //  var dateTo = oDateFormat.format(Created_On);
 					
-		       	dateTo = dateTo;
-					var dataTo1 = Number(dateTo.replace(/[^A-Z\d\s]/gi, '').replace(/[^0-9\.]+/g, ""));
+		   //    	dateTo = dateTo;
+					// var dataTo1 = Number(dateTo.replace(/[^A-Z\d\s]/gi, '').replace(/[^0-9\.]+/g, ""));
 			
-					var ValidTo = new Date(dataTo1).toUTCString();
-					var dateAsReceived = moment.tz(ValidTo, "GMT");
-					var returnThisDate = moment(dateAsReceived).format('YYYY-MM-DD');
+					// var ValidTo = new Date(dataTo1).toUTCString();
+					// var dateAsReceived = moment.tz(ValidTo, "GMT");
+					// var returnThisDate = moment(dateAsReceived).format('YYYY-MM-DD');
 					return returnThisDate;
 			},
 			
